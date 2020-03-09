664b8b72855ac090ac5c2d59ee2ec86592eb9dfa
Julianne David <juliannedavid@gmail.com>
2020-03-09 11:34:02 -0700

    Adds option for db generation from scratch after incomplete index

--- junction_database/jx_indexer.py
+++ junction_database/jx_indexer.py
@@ -32,8 +32,33 @@ if __name__ == '__main__':
     log_mode = args.log_level
     testing = args.testing
 
+    db_name = os.path.join(db_path, 'new_jx_index.db')
+    if args.subparser_name == 'index' and os.path.exists(db_name):
+        options = ['y', 'n']
+        user_check = input(
+            '\nThe database file already exists. Would you like to erase the '
+            'current database file and begin generating the database from '
+            'scratch? [y/n]\n(Note: the full database is 249GB and takes '
+            'significant resources to generate. Deleting the database file is '
+            'only recommended if a previous indexing run did not complete '
+            'successfully.\nWould you like to delete the database file? '
+            '[y/n]: '
+        )
+        while (user_check not in options):
+            user_check = input(
+                "Your response isn't recognized. Please enter 'y' if you "
+                "would like to delete the existing database file and generate "
+                "the database from scratch and 'n' otherwise: "
+            )
+        if user_check == 'n':
+            print('Database will not be generated.  Exiting.')
+            exit()
+
+        else:
+            print('Deleting existing database file.')
+            os.remove(db_name)
+
     try:
-        db_name = os.path.join(db_path, 'new_jx_index.db')
         conn = sql.connect(db_name)
         index_db = conn.cursor()
     except sql.OperationalError:
